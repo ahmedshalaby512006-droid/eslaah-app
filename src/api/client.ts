c12c9c76443903dc2579c519e18cee871910a448
ahmedshalaby512006-droid <ahmedshalaby512006@gmail.com>
2026-09-05 19:31:20 +0300

    Update client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://eslaah-core-production.up.railway.app',
+baseURL: import.meta.env.VITE_API_URL || 'https://eslaah-core-production-bfca.up.railway.app',
   headers: {
     'Content-Type': 'application/json',
   },
